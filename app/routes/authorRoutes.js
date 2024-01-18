92442537be9b30e445e527b09108f0de03005893
Talon Singletary <twsingletary@student.fullsail.com>
2024-01-18 01:20:00 -0600

    adding week 2 thus far

---
+++ app/routes/authorRoutes.js
@@ -0,0 +1,20 @@
+const router = require("express").Router();
+const {
+    createAuthor,
+    getAllAuthors,
+    getAuthorById,
+    updateAuthor,
+    deleteAuthor,
+} = require("../controller/authorController");
+
+router.get("/", getAllAuthors);
+
+router.get("/:id", getAuthorById);
+
+router.post("/", createAuthor);
+
+router.put("/:id", updateAuthor);
+
+router.delete("/:id", deleteAuthor);
+
+module.exports = router;
